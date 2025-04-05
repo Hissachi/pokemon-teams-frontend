50f229c9e5d8f20dccf9cb35ed6d54274c5b5471
Hissachi <lucashissachi@gmail.com>
2025-04-05 16:56:29 -0300

    feat: criação da page dashboard quando fizer login

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,160 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Time Alfa",
+      logo: GalleryVerticalEnd,
+      plan: "descrição",
+    },
+    {
+      name: "Time Beta",
+      logo: AudioWaveform,
+      plan: "descrição",
+    },
+    {
+      name: "Time Gama",
+      logo: Command,
+      plan: "descrição",
+    },
+  ],
+  navMain: [
+    {
+      title: "Meu Time",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Gerenciar times",
+          url: "#",
+        },
+        {
+          title: "Criar novo time",
+          url: "#",
+        },
+        {
+          title: "Excluir time",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Pokédex",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Seus pokémons",
+          url: "#",
+        },
+        {
+          title: "Todos os pokémons",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Ajuda",
+      url: "#",
+      icon: BookOpen,
+      items: [
+        {
+          title: "Sobre",
+          url: "#",
+        },
+        {
+          title: "FAQ",
+          url: "#",
+        },
+        {
+          title: "Contato",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Configurações",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Geral",
+          url: "#",
+        },
+        {
+          title: "Times",
+          url: "#",
+        },
+        {
+          title: "Perfil",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "Design Engineering",
+      url: "#",
+      icon: Frame,
+    },
+    {
+      name: "Sales & Marketing",
+      url: "#",
+      icon: PieChart,
+    },
+    {
+      name: "Travel",
+      url: "#",
+      icon: Map,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -58,9 +58,11 @@ export function LoginForm({
                 </div>
                 <Input id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full">
-                Entrar
-              </Button>
+              <Link href="/dashboard" className="w-full">
+                <Button className="w-full">
+                  Entrar
+                </Button>
+              </Link>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
                   Ou cadastre-se com:
